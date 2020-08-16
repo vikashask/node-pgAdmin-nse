8ab99e2eec2d9f7a1c799c8f91c6ff2ce11eafca
vikash <vikashraj144@gmail.com>
2020-08-16 16:41:43 +0530

    addStockData stock data

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -1,12 +1,12 @@
 var knex = require("../config/knex");
 const path = require('path');
 var fs = require('fs');
-
+const httpStatus = require('../utils/httpStatus');
 
 module.exports.test = async (req, res) => {
-  // var data = await knex("test2").select('name');
-  // console.log("module.exports.test -> data", data)
-  // return res.status(httpStatus.OK).json(data);
+  var data = await knex("test2").select('name');
+  console.log("module.exports.test -> data", data)
+  return res.status(httpStatus.OK).json(data);
 
   // fs.readFile('faomktstats20191011.txt', 'utf8', function (err, data) {
   //   if (err) throw err;
@@ -36,17 +36,41 @@ module.exports.addStockData = async (req, res) => {
   csv()
     .fromFile(csvFilePath)
     .then((jsonObj) => {
-      // console.log(jsonObj);
-      let insert_sql = `INSERT INTO public.stock_data(
-      "checkSum", exp, filler, "instName", "mktType", "msgLen", "openInt", "optType", "secToken", "strikePrice", symbol, "timeStamp", transcode, field14)
-      VALUES `
-      for (let index = 0; index < 5; index++) {
+      let insert_sql = `INSERT INTO stock_nse(
+        "Transcode", "TimeStamp", "MsgLen", "SecToken", "InstName", "Symbol", "Exp", "StrikePrice", "OptType", "OpenInt", "MktType", "CheckSum", "Filler")
+      VALUES `;
+      for (let index = 0; index < jsonObj.length; index++) {
         const element = jsonObj[index];
-        insert_sql = `(${element.Transcode}, ${element.TimeStamp}, ${element.MsgLen}, ${element.SecToken}, ${element.InstName}, ${element.Symbol}, 
-        ${element.Exp}, ${element.StrikePrice}, ${element.OptType}, ${element.OpenInt}, ${element.MktType}, ${element.CheckSum}, ${element.Filler}, ${element.field14}),`
-
+        // console.log("module.exports.addStockData -> element.Filler", element.Filler.length)
+        console.log("module.exports.addStockData -> typeof element.InstName;", typeof element.InstName,element.InstName,JSON.stringify(element.InstName).replace('"', "'").replace('"', "'"));
+        element.Transcode = element.Transcode?element.Transcode:'';
+        element.TimeStamp = element.TimeStamp?element.TimeStamp:'';
+        element.MsgLen =element.MsgLen ?element.MsgLen:'';
+        element.SecToken=element.SecToken?element.SecToken:'';
+        element.InstName=element.InstName?JSON.stringify(element.InstName).replace('"', "'").replace('"', "'"):'';
+        element.Symbol=element.Symbol?JSON.stringify(element.Symbol).replace('"', "'").replace('"', "'"):'';
+        element.Exp=element.Exp?JSON.stringify(element.Exp).replace('"', "'").replace('"', "'"):'';
+        element.StrikePrice=element.StrikePrice?element.StrikePrice:'';
+        element.OptType=element.OptType?JSON.stringify(element.OptType).replace('"', "'").replace('"', "'"):'';
+        element.OpenInt=element.OpenInt?element.OpenInt:'';
+        element.MktType=element.MktType?JSON.stringify(element.MktType).replace('"', "'").replace('"', "'"):'';
+        element.CheckSum=element.CheckSum?element.CheckSum:'';
+        element.Filler=element.Filler.length>1?JSON.stringify(element.Filler).replace('"', "'").replace('"', "'"):"''";
+        insert_sql += `(${element.Transcode}, ${element.TimeStamp}, ${element.MsgLen}, ${element.SecToken}, ${element.InstName}, ${element.Symbol}, 
+        ${element.Exp}, ${element.StrikePrice}, ${element.OptType}, ${element.OpenInt}, ${element.MktType}, ${element.CheckSum}, ${element.Filler})`
+          // if(index ===50-1){
+            if(index ===jsonObj.length-1){
+            insert_sql +='  ';
+          }else{
+            insert_sql +=' , ';
+          }
       }
-      console.log("module.exports.addStockData -> element", insert_sql)
-
+    // var data = await knex.raw(insert_sql);
+    knex.raw(insert_sql).then(function(resp) { 
+      // console.log('resp ----------',resp);
+      return res.json({
+                response: 'insertion done'
+              });
+    });
     })
-}
+};
